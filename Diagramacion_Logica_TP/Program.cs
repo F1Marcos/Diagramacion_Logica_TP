e99920f7e2204484d46509916c38f7ee36a47a28
mquispe <mquispe@MQUISPE-AR.ar.oracle.com>
2019-08-14 19:00:09 -0300

    Add project files.

---
+++ Diagramacion_Logica_TP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diagramacion_Logica_TP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
